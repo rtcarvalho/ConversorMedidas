4f82db302fd485081765bdf0fe17d372805fe4de
Raphael Torres <60902694+rtcarvalho@users.noreply.github.com>
2022-03-26 17:26:43 -0300

    Calcula medidas solicitadas

---
+++ main.py
@@ -0,0 +1,17 @@
+medida = float(input("ENTRADA: "))
+milhas = medida * 1
+polegadas = medida * 2.54
+pes = medida * 31.48
+centimetros = medida *160934
+metros = medida * 1609
+quilometros = medida * 1.6
+
+print("SAIDA")
+print("{} Milha(s) = {} Milha(s)".format(medida, milhas))
+print("{} Milha(s) = {} Polegada(s)".format(medida, polegadas))
+print("{} Milha(s) = {} Pé(s)".format(medida, pes))
+print("{} Milha(s) = {} Centimetro(s)".format(medida, centimetros))
+print("{} Milha(s) = {} Metro(s)".format(medida, metros))
+print("{} Milha(s) = {} Quilometro(s)".format(medida, quilometros))
+
+
